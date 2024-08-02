06160a3519ff1ddb589bd7b9588320a48b69100e
Ismael Garcia <melgarciacas@gmail.com>
2024-08-02 15:51:58 -0500

    Version corregida del bug 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
 import { useState } from "react";
-import Header from "./components/Header";
+{/*Impoting Components */}
 import Tracks from "./components/Tracks";
 import Driver from "./components/Driver";
 import Team from "./components/Team";
-import "../src/app.css";
 import Recap from "./components/Recap";
-import spa from './assets/spa.png'
-import lando from './assets/lando.png'
+{/*Importing Css Styles */}
+import "../src/app.css";
+{/*Importing images directly */}
+import f1_logo from './assets/f1_logo.svg';
+import monaco from './assets/monaco.png';
+import silverstone from './assets/silverstone.png';
+import spa from './assets/spa.png';
+import max from './assets/max.png';
+import lando from './assets/lando.png';
+import charles from './assets/charles.png';
+import ferrari from './assets/ferrari.jpg';
+import mercedes from './assets/mercedes.jpeg';
+import mclaren from './assets/mclaren.jpg';
+import redbull from './assets/redbull.png';
+
 export default App;
 
 function App() {
@@ -14,21 +26,23 @@ function App() {
     <div className="main-container">
       <div className="header-section">
         {/*Top Header class, the header was created as a component*/}
-        <Header />
+        <div className='header-top'>
+            <img src={f1_logo}alt="logo" />
+        </div>
       </div>
       <h2 className="iconic-tracks-h2">Most Iconic F1 Circuits</h2>
       <div className="iconic-tracks">
         {/* The Tracks component shows each track card according to the information of the circuit, name, image, and description*/}
         <Tracks
           name={"MONACO"}
-          image={"../src/assets/monaco.png"}
+          image={monaco}
           desc={
             "The Monaco Grand Prix is a legendary F1 circuit, known for its narrow streets, tight corners, and stunning Monte Carlo backdrop, challenging driver precision and skill."
           }
         />
         <Tracks
           name={"SILVERSTONE"}
-          image={"./src/assets/silverstone.png"}
+          image={silverstone}
           desc={
             "Silverstone Circuit is a historic F1 track in the UK, known for its high-speed straights and iconic corners, offering a thrilling challenge for drivers."
           }
@@ -47,7 +61,7 @@ function App() {
           <h2>Top 2024 Drivers</h2>
           <Driver
             driver={"Max Verstappen"}
-            avatar={"./assets/max.png"}
+            avatar={max}
             teammember={"Red Bull Racing"}
             stats={"https://www.formula1.com/en/drivers/max-verstappen"}
           />
@@ -59,7 +73,7 @@ function App() {
           />
           <Driver
             driver={"Charles Leclerc"}
-            avatar={"./assets/charles.png"}
+            avatar={charles}
             teammember={"Ferrari"}
             stats={"https://www.formula1.com/en/drivers/charles-leclerc"}
           />
@@ -69,25 +83,25 @@ function App() {
           <h2>"The Big 4" Performance in 2024</h2>
           <Team
             teamname={"Ferrari"}
-            logo={"./src/assets/ferrari.jpg"}
+            logo={ferrari}
             score={"345pts"}
             story={"https://www.formula1.com/en/teams/ferrari"}
           />
           <Team
             teamname={"Mercedes"}
-            logo={"./src/assets/mercedes.jpeg"}
+            logo={mercedes}
             score={"266pts"}
             story={"https://www.formula1.com/en/teams/mercedes"}
           />
           <Team
             teamname={"McLaren"}
-            logo={"./src/assets/mclaren.jpg"}
+            logo={mclaren}
             score={"366pts"}
             story={"https://www.formula1.com/en/teams/mclaren"}
           />
           <Team
             teamname={"Red Bull"}
-            logo={"./src/assets/redbull.png"}
+            logo={redbull}
             score={"408pts"}
             story={"https://www.formula1.com/en/teams/red-bull-racing"}
           />
@@ -99,7 +113,7 @@ function App() {
           <Recap url={'https://www.youtube.com/watch?v=pQaG0ay957M'}/>
       </div>
       <div className="footer">
-          <img src="src/assets/f1_logo.svg" alt="logo" className="final-logo" />
+          <img src={f1_logo} alt="logo" className="final-logo" />
           <div className="footer-text">
         <span>Ismael Garcia | F1 Theme Page Using ReactJS & Vite</span>
           <br />
--- src/components/Header.jsx
+++
@@ -1,9 +0,0 @@
-import '../app.css';
-export default Header;
-function Header(){
-    return(
-        <div className='header-top'>
-            <img src="src/assets/f1_logo.svg" alt="logo" />
-        </div>
-    );
-}
